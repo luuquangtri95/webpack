c0417e4385fe6f8c76b07f20cbef2776a9943b28
luuquangtri95 <luuquangtri95@gmail.com>
2022-11-06 18:10:30 +0700

    init prj and first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from "./utils";
+import "./styles/style.css";
+import "./styles/style.scss";
+
+console.log(sum(12, 12));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: path.resolve("src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //do not use both MiniCssExtractPlugin + style-loader => conflict => should be 1 or 2
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
